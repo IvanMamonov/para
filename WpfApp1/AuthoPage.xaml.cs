d9a4b410427ad3a2be3bf0a78d546a1e89a6b3cb
Иван Мамонов <mamonov_vanya03@mail.ru>
2023-01-16 19:57:54 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/AuthoPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthoPage.xaml
+    /// </summary>
+    public partial class AuthoPage : Page
+    {
+        public static ObservableCollection<User> user { get; set; }
+        public AuthoPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            user = new ObservableCollection<User>(db_connection.conection.User.ToList());
+            var z = user.Where(s => s.Login == txt_login.Text && s.Password == txt_passwors.Password).FirstOrDefault();
+            if (z != null)
+            {
+                NavigationService.Navigate(new HomePage());
+            }
+            else
+            {
+                MessageBox.Show("логин или пароль не верные", "error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new RegPage());
+        }
+    }
+}
---
+++ WpfApp1/RegPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для RegPage.xaml
+    /// </summary>
+    public partial class RegPage : Page
+    {
+        public static ObservableCollection<User> users { get; set; }
+        public RegPage()
+        {
+            InitializeComponent();
+            users = new ObservableCollection<User>(db_connection.conection.User.ToList());
+        }
+
+        private void Button_Click_Reg(object sender, RoutedEventArgs e)
+        {
+            var a = new User();
+            a.Login = txt_login.Text;
+            a.Password = txt_passwors.Password;
+            db_connection.conection.User.Add(a);
+            db_connection.conection.SaveChanges();
+            NavigationService.GoBack();
+            NavigationService.Navigate(new HomePage());
+        }
+
+        private void Button_Click_Back(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
